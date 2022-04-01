6fc1d0c2f1cbaeb22817ea0d5c054fdf5c52486c
Julien Barbot <jubarbot@cisco.com>
2022-04-01 18:39:57 +0200

    Bump version

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -1,8 +1,8 @@
 class Haproxy < Formula
     desc "Reliable, high performance TCP/HTTP load balancer"
     homepage "https://www.haproxy.org/"
-    url "https://www.haproxy.org/download/2.2/src/haproxy-2.2.10.tar.gz"
-    sha256 "a027e9cd8f703ba48dc193f5ae34d9aa152221f67ab58a4e939c96b9f4edd3bc"
+    url "https://www.haproxy.org/download/2.5/src/haproxy-2.5.5.tar.gz"
+    sha256 "063c4845cdb2d76f292ef44d9c0117a853d8d10ae5d9615b406b14a4d74fe4b9"
     depends_on "openssl@1.1"
     depends_on "pcre"
     depends_on "lua"
